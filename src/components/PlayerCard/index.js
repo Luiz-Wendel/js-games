6d9c7612b1c96a4fb20b39be22836e3ba212559f
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-09-07 14:09:28 -0300

    feat: add player card

---
+++ src/components/PlayerCard/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PlayerCard = ({ id, title, image }) => {
+  return (
+    <section id={ id }>
+      <h2>{ title }</h2>
+      <span>{ image }</span>
+    </section>
+  )
+}
+
+export default PlayerCard
--- src/pages/TicTacToe/index.js
+++ src/pages/TicTacToe/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import PlayerCard from '../../components/PlayerCard';
 
 class TicTacToe extends React.Component {
   render() {
     return (
       <main>
         <h1>Tic Tac Toe</h1>
+        <section>
+          <PlayerCard id="p1" title="Player 1" image="X" />
+          <PlayerCard id="p2" title="Player 2" image="O" />
+        </section>
       </main>
     )
   }
