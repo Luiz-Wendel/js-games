3a3c7873d87d87de45f4d2119d439a4d6efdecf2
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-09-07 14:05:46 -0300

    feat: add Tic Tac Toe page

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
+import TicTacToe from '../pages/TicTacToe';
 
 class Routes extends React.Component {
   render() {
     return (
       <Switch>
         <Route exact path='/' component={ Home } />
+        <Route exact path='/tic-tac-toe' component={ TicTacToe } />
         <Route component={ NotFound } />
       </Switch>
     )
