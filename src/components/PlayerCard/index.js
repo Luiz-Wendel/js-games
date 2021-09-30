74312affdf3087139ec16885c666a2e667801462
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-09-30 15:44:38 -0300

    feat: add score to player card

--- src/components/PlayerCard/index.js
+++ src/components/PlayerCard/index.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 const PlayerCard = ({ id, title, image }) => {
+  const { score } = useSelector(({ ticTacToe }) => ticTacToe);
+
   return (
     <section id={ id }>
       <h2>{ title }</h2>
       { image }
+      <span>{ score[id === 'p1' ? 1 : 2] }</span>
     </section>
-  )
+  );
 };
 
-export default PlayerCard
+export default PlayerCard;
