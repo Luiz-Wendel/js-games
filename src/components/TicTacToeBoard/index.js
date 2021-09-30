728acadf33c069994c3d344855576f5e4eb7afae
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-09-30 14:50:53 -0300

    feat: add victory check function

--- src/components/TicTacToeBoard/index.js
+++ src/components/TicTacToeBoard/index.js
@@ -1,13 +1,57 @@
 import React from 'react';
 import TicTacToeRow from '../TicTacToeRow';
 import style from './style.module.css';
-import getNumbersFromString from '../../helpers/utils';
+import { getNumbersFromString, reverseMatrix } from '../../helpers/utils';
 import { useDispatch, useSelector } from 'react-redux';
 import { playerMove } from '../../actions';
+
+const horizontalVictory = (gameState) => {
+  let hasVictory = 0;
+
+  gameState.forEach((row) => {
+    const rowSet = [...new Set(row)];
+
+    if (rowSet[0] !== 0 && rowSet.length === 1) hasVictory = rowSet[0];
+  });
+
+  return hasVictory;
+};
+
+const verticalVictory = (gameState) => {
+  const reversedGameState = reverseMatrix(gameState);
+
+  return horizontalVictory(reversedGameState);
+};
+
+const diagonalVictory = (gameState) => {
+  const diagonals = [[[0, 0], [1, 1], [2, 2]], [[0, 2], [1, 1], [2, 0]]];
+  const diagonalGameState = [];
+
+  diagonals.forEach((diagonal) => {
+    const newRow = [];
+
+    diagonal.forEach(([row, column]) => {
+      newRow.push(gameState[row][column]);
+    })
+
+    diagonalGameState.push(newRow);
+  });
+
+  return horizontalVictory(diagonalGameState);
+
+};
+
+const checkVictory = (gameState) => horizontalVictory(gameState)
+  || verticalVictory(gameState)
+  || diagonalVictory(gameState);
 
 const TicTacToeBoard = () => {
   const dispatch = useDispatch();
   const { gameState, playerTurn } = useSelector(({ ticTacToe }) => ticTacToe);
+
+  React.useEffect(() => {
+    console.log(checkVictory(gameState));
+  }, [gameState]);
 
   const handlePlayerTurn = (row, column, target) => {
     const payload = {
--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,3 +1,17 @@
-const getNumbersFromString = (string) => string.replace(/\D/g, '');
+export const getNumbersFromString = (string) => string.replace(/\D/g, '');
 
-export default getNumbersFromString;
+export const reverseMatrix = (matrix) => {
+  const reversedMatrix = [];
+
+  for (let column = 0; column < matrix[0].length; column += 1) {
+    const newRow = [];
+
+    for (let row = 0; row < matrix.length; row += 1) {
+      newRow.push(matrix[row][column]);
+    }
+
+    reversedMatrix.push(newRow);
+  }
+
+  return reversedMatrix;
+};
