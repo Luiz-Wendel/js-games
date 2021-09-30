85cb8146973001c28bad72d6b20b0c01a5e10ddf
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-09-30 17:08:50 -0300

    feat: add game card on home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import GameCard from '../../components/GameCard';
 
 class Home extends React.Component {
   render() {
     return (
       <main>
         <h1>JS Games</h1>
+        <section>
+          <GameCard title="TicTacToe" path="/tic-tac-toe" />
+        </section>
       </main>
     );
   }
